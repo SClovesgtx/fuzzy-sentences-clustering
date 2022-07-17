281c71635020eb7938832b91453269a69197a0b1
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2022-07-17 06:53:41 -0300

    basic functions and requirements

---
+++ phrases-fuzzy-clusters/phrases_fuzzy_clusters/phrases_fuzzy_clusters.py
@@ -0,0 +1,72 @@
+from cmath import phase
+import nltk
+from fuzzywuzzy import fuzz
+
+try:
+    stemmer = nltk.stem.RSLPStemmer()
+    stopwords = nltk.corpus.stopwords.words("portuguese")
+    tokenizer = nltk.tokenize.word_tokenize
+except:
+    nltk.download("rslp")
+    nltk.download("stopwords")
+    nltk.download("punkt")
+    stemmer = nltk.stem.RSLPStemmer()
+    stopwords = nltk.corpus.stopwords.words("portuguese")
+    tokenizer = nltk.tokenize.word_tokenize
+
+def clean_token(token):
+    new_token = "".join([s for s in token if s.isalpha()])
+    return new_token
+
+
+def is_valid_token(token):
+    is_valid = False
+    if token not in stopwords:
+       is_valid = True
+    return is_valid
+
+def apply_stemmer(token):
+    new_token = clean_token(token=token)
+    if new_token:
+        return stemmer.stem(new_token)
+    return ""
+
+def split_into_tokens(phrase):
+    lower_case_phrase = phrase.lower()
+    tokens = tokenizer(lower_case_phrase, language="portuguese")
+    clean_tokens = [apply_stemmer(token) for token in tokens if is_valid_token(token)]
+    return clean_tokens
+
+
+def make_corpus(phrases):
+    tokenized_corpus = []
+    for phrase in phrases:
+        tokenized_phrase = split_into_tokens(phrase=phrase)
+        tokenized_corpus.append((phrase, tokenized_phrase))
+    return tokenized_corpus
+
+def group_by_clusters(phrases, similarity_threshold=95):
+    tokenized_corpus = make_corpus(phrases)
+    has_cluster = []
+    clusters = [[phrase, -1] for phrase in phrases]
+    cluster_id = 0
+    for i, i_item in enumerate(tokenized_corpus):
+        if i_item not in has_cluster:
+            new_idx = True
+            for j, j_item in enumerate(tokenized_corpus):
+                if i != j and j_item[1] not in has_cluster:
+                    similarity = fuzz.partial_ratio(i_item[1], j_item[1])
+                    if similarity >= similarity_threshold:
+                        if new_idx:
+                            cluster_id += 1
+                            clusters[i][1] = cluster_id
+                            has_cluster.append(i_item[1])
+                            new_idx = False
+                        clusters[j][1] = cluster_id
+                        has_cluster.append(j_item[1])
+    return clusters
+
+
+phrases = ["eu morava em florianópolis", "eu moro em florianópolis", "quero comprar um carro", "quero compra de um carro"]
+res = group_by_clusters(phrases)
+print(res)
