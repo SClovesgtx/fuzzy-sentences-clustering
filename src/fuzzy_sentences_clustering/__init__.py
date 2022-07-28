b6f3c0a0ba51804775fc9df3cdf6f122decd9889
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2022-07-28 12:05:52 -0300

    feat: new languages and similarity methods

--- src/fuzzy_sentences_clustering/__init__.py
+++ src/fuzzy_sentences_clustering/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "0.0.1"
+__version__ = "1.1.1"
 from .fuzzy_sentences_clustering import *
--- src/tests/test_fuzzy_sentences_clustering.py
+++ src/tests/test_fuzzy_sentences_clustering.py
@@ -1,14 +1,32 @@
 from fuzzy_sentences_clustering import *
+from fuzzy_sentences_clustering import __version__
 
 
-def test_look_for_clusters():
+def test_check_version():
+    assert __version__ == "1.1.1"
+
+
+def test_look_for_clusters_en():
     sentences = [
-        "morava em florianópolis",
-        "comprar um carro",
-        "compra de um carro",
-        "em florianópolis eu moro",
-        "gosto de samba",
-        "quero comer tapioca",
+        "I live in New York",
+        "I want to buy a car",
+        "a car I would like to buy",
+        "Ohh New York, I lived there in 2005",
+        "I have a dog",
     ]
-    res = look_for_clusters(sentences)
-    assert res == [1, 2, 2, 1, -1, -1]
+    res = look_for_clusters(sentences, similarity_threshold=60)
+    assert res == [1, 2, 2, 1, -1]
+
+
+def test_look_for_clusters_ger():
+    sentences = [
+        "ich lebe in New York",
+        "Ich möchte ein Auto kaufen",
+        "ein Auto, das ich kaufen möchte",
+        "Oh New York, Ich habe dort 2005 gelebt",
+        "Ich habe einen Hund",
+    ]
+    res = look_for_clusters(
+        sentences, language="german", method="token_set_ratio", similarity_threshold=80
+    )
+    assert res == [1, 2, 2, 1, -1]
